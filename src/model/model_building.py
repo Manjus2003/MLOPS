0e1e3984c52e5c5cec8310645978c85bcbc239fe
Manjus2003 <manjus2003@github.com>
2026-02-23 16:58:21 +0530

    updtated cicd-final ecr1

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -13,7 +13,7 @@ def load_data(file_path: str) -> pd.DataFrame:
         logging.info('Data loaded from %s', file_path)
         return df
     except pd.errors.ParserError as e:
-        logging.error('Failed to parse the CSV file: %s', e)
+        logging.error('Failed to parse the CSV file.: %s', e)
         raise
     except Exception as e:
         logging.error('Unexpected error occurred while loading the data: %s', e)
